9ef713a369960d8eded0d3f0a3ebc4f2ebfbfe3e
zhelonkinaleksei <120109097+zhelonkinaleksei@users.noreply.github.com>
2023-03-06 19:49:57 +0300

    1

--- js/calc.js
+++ js/calc.js
@@ -1,7 +1,21 @@
 let calculationButton = document.querySelector('.calculation__button');
-let countQuest = Number(document.getElementById('countquest').value);
 let countsum = document.getElementById('countsum');
-calculationButton.addEventListener('click', function(){
-    let result = countQuest;
-    countsum.innerHTML = result;
-})
+calculationButton.onclick =  function(){
+    let countQuest = Number(document.getElementById('countquest').value);
+    let countChild = Number(document.getElementById('countchild').value);
+    let countAdult = Number(document.getElementById('countadult').value);
+    let countHour = Number(document.getElementById('counthour').value);
+    let resultsum = countQuest*2000; 
+    if(countQuest > 200){
+        alert('Количество гостей не может быть больше 200');
+    }
+    else{
+
+    countsum.innerHTML = resultsum;
+    }
+    
+   
+};
+
+
+    
